7b58d4dae18e4239f62b7f39b6b8e562dc638a8b
Mr Speaker <mrspeaker@gmail.com>
2014-05-10 12:21:10 -0400

    es6 it up

--- script/main.js
+++ script/main.js
@@ -8,65 +8,55 @@
 
 		init: function () {
 
-			this.canvas = document.querySelector("#board");
-			this.ctx = this.canvas.getContext("2d");
-			this.w = this.canvas.width;
-			this.h = this.canvas.height;
-			this.imgData = this.ctx.createImageData(this.w, this.h);
+			this.initCanvas();
 
 			this.env = {
 				w: this.w,
 				h: this.h,
-				time: 0
+				t: 0
 			};
-
-			var env = this.env;
 
 			// "Shaders"
 			this.funcs.push(
 
 				// Clear
-				function (val, p) {
-					return 0;
+				(val, p) => 0,
+
+				// Sin/cos swishy
+				(val, p, e) => {
+					let {t} = e
+					let s = val
+					s += (p.x * (Math.sin(t / 30) * 3)) + (p.y * (Math.cos(t / 50) * 2));
+					s += (p.x * (Math.sin(t / 50) * 3)) + (p.y * (Math.cos(t / 100) * 2))
+					return s;
 				},
 
 				// Circles
-				function (val, p) {
-					var xo = (env.w / 2) - p.x + (Math.sin(env.time / 100) * 7),
-						yo = (env.h / 2) - p.y + (Math.cos(env.time / 100) * 7),
-						dist = Math.sqrt(xo * xo + yo * yo);
+				(val, p, e) => {
+					let xo = (e.w / 2) - p.x + (Math.sin(e.t / 100) * 7),
+						yo = (e.h / 2) - p.y + (Math.cos(e.t / 100) * 7),
+						dist = Math.hypot(xo, yo);
 
-					var outer = dist > (Math.abs(Math.sin(env.time / 100))) * 15 ? 60 : 0;
-					var inner = dist * 1.5 > (Math.abs(Math.sin(env.time / 150))) * 30 ? 60 : 0;
+					let outer = dist > (Math.abs(Math.sin(e.t / 100))) * 15 ? 60 : 0;
+					let inner = dist * 1.5 > (Math.abs(Math.sin(e.t / 150))) * 30 ? 60 : 0;
 
 					return val + ((outer + inner) / 2);
 				},
 
 				// Waves
-				function (val, p) {
-					return val + (p.x * ((p.idx + (env.time / 3 | 0)) % 10) + p.y) * 0.5;
-				},
-
-				// Sine swishy
-				function (val, p) {
-					var siney = (p.x * (Math.sin(env.time / 30) * 3)) + (p.y * (Math.cos(env.time / 50) * 2));
-					var siney1 = siney + (p.x * (Math.sin(env.time / 50) * 3)) + (p.y * (Math.cos(env.time / 100) * 2))
-					return val + siney1;
-				},
+				(val, p, e) => val + (p.x * ((p.idx + (e.t / 3 | 0)) % 10) + p.y) * 0.1,
 
 				// Sine dot
-				function (val, p) {
-					return val + ((p.x == (env.time / 5 % 32 | 0) &&
-						p.y == 16 + (Math.sin(env.time / 8) * 8 | 0)) ?
-							255 : 0);
-				},
+				(val, p, e) => (
+					(p.x == (e.t / 5 % e.w | 0)) &&
+					(p.y == 16 + (Math.sin(e.t / 8) * 8 | 0))
+					? 255 : val),
 
 				// Sine dot stalker
-				function (val, p) {
-					return val + ((p.x == ((env.time - 4) / 5 % 32 | 0) &&
-						p.y == 16 + (Math.sin((env.time - 4) / 8) * 8 | 0)) ?
-							100 : 0);
-				}
+				(val, p, e) => (
+					(p.x == ((e.t - 4) / 5 % e.w | 0)) &&
+					(p.y == 16 + (Math.sin((e.t - 4) / 8) * 8 | 0))
+					? 100 : val)
 
 			);
 
@@ -74,17 +64,22 @@
 
 		},
 
+		initCanvas: function () {
+			this.canvas = document.querySelector("#board");
+			this.ctx = this.canvas.getContext("2d");
+			this.w = this.canvas.width;
+			this.h = this.canvas.height;
+			this.imgData = this.ctx.createImageData(this.w, this.h);
+		},
+
 		run: function () {
 			this.update();
 			this.render();
-			setTimeout(function () {
-				this.run();
-			}.bind(this), 16);
+			setTimeout(() => this.run(), 16);
 		},
 
 		putPixel: function (col, x, y) {
-
-			var idx = (y * this.w + x) * 4,
+			let idx = (y * this.w + x) * 4,
 				img = this.imgData.data;
 
 			col = Math.min(255, Math.max(0, col));
@@ -93,32 +88,29 @@
 			img[idx + 1] = col;
 			img[idx + 2] = col;
 			img[idx + 3] = 255;
-
 		},
 
 		update: function () {
-
-			var self = this,
-				img = this.imgData.data;
+			let {imgData, env, w, h} = this,
+				img = imgData.data;
 
 			// Set up the field
-			for (var i = 0; i < this.h; i++) {
-				for (var j = 0; j < this.w; j++) {
-					var index = (j + (i * this.w)) * 4;
+			for (let y = 0; y < h; y++) {
+				for (let x = 0; x < w; x++) {
+					let idx = (x + (y * w)) * 4,
+						data = { idx: idx, x: x, y: y, xr: x / w, yr: y / h };
 
-					var mixed = this.funcs.reduce(function (ac, f) {
-						return f(
-							ac,
-							{idx: index, x: j, y: i, xr: j / 32, yr: j / 32}
-						);
-					}, img[index]);
+					let mixed = this.funcs.reduce((ac, f) => f(
+						Math.min(255, Math.max(0, ac)),
+						data,
+						env
+					), img[idx]);
 
-					self.putPixel(mixed, j, i);
-
+					this.putPixel(mixed, x, y);
 				}
 			}
 
-			this.env.time++;
+			env.t++;
 		},
 
 		render: function () {
