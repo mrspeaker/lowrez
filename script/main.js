df832d846ba53dc6815d9c7f82fb13e5fd5e1cc6
Mr Speaker <mrspeaker@gmail.com>
2014-05-13 21:45:29 -0400

    glsl-ify

--- script/main.js
+++ script/main.js
@@ -16,53 +16,59 @@
 				t: 0
 			};
 
+			let add = ([r,g,b], c) => [r + c, g + c, b + c];
+
 			// "Shaders"
 			let shaders = [
 
 				// Clear
-				() => 0,
+				() => [0, 0, 0],
 
 				// Noise
-				// (val) => val + Math.random() * 0.4,
+				//(val) => add(val, Math.random() * 0.2),
 
 				// Sin/cos swishy
-				(val, {xr, yr}, {t}) => {
+				([r, g, b], {xr, yr}, {t}) => {
 					let {sin, cos} = Math,
-						s = val;
+						s = 0;
+
 					s += sin(xr * cos(t / 300) * 80) + cos(yr * cos(t / 300) * 10);
 					s += sin(yr * sin(t / 200) * 40) + cos(xr * sin(t / 500) * 40);
 					s += sin(xr * sin(t / 100) * 10) + sin(yr * sin(t / 600) * 80);
 					s *= sin(t / 200) * 0.5;
-					return s;
+
+					return [r + s, g + (s * 0.5), b + (sin(s + t / 30) * 0.75)];
 				},
 
 				// Waves
-				(val, {x, y, idx}, {t}) => val + (x * ((idx + (t / 3 | 0)) % 10) + y) * 0.0002,
+				// (val, {x, y, idx}, {t}) => add(val, (x * ((idx + (t / 3 | 0)) % 15) + y) * 0.0002),
 
 				// Circles
 				(val, {xr, yr}, {t}) => {
 					let {sin, cos, abs, hypot} = Math,
-						xo = 0.5 - xr + (sin(t / 100) / 3),
-						yo = 0.5 - yr + (cos(t / 80) / 3),
+						xo = 0.5 - xr + (sin(t / 100) / 3) * 2,
+						yo = 0.5 - yr + (cos(t / 80) / 3) * 2,
 						dist = hypot(xo, yo);
 
 					let outer = dist * 2.3 < (abs(sin(t / 100))) * 0.7 ? 0.3 : 0;
 					let inner = dist * 2.7 < (abs(sin(t / 100))) * 0.6 ? 0.8 : 0;
 
-					return val + ((outer + inner) / 2);
+					return add(val, ((outer + inner) / 2));
 				},
 
+				/*
 				// Sine dot
-				(val, {x, y}, {w, t}) => (
+				(val, {x, y}, {w, h, t}) => (
 					(x == (t / 5 % (w * 2) | 0)) &&
-					(y == 16 + (Math.sin(t / 8) * 8 | 0))
-					? 1 : val),
+					(y == h / 2 + (Math.sin(t / 8) * 8 | 0))
+					? [1, 1, 1] : val),
 
 				// Sine dot stalker
-				(val, {x, y}, {w, t}) => (
+				(val, {x, y}, {w, h, t}) => (
 					(x == ((t - 4) / 5 % (w * 2) | 0)) &&
-					(y == 16 + (Math.sin((t - 4) / 8) * 8 | 0))
-					? 0.8 : val)
+					(y == h / 2 + (Math.sin((t - 2) / 8) * 8 | 0))
+					? [0.8, 0.8, 0.8] : val)
+				*/
 
 			];
 
@@ -86,33 +92,34 @@
 			setTimeout(() => this.run(), 16);
 		},
 
-		putPixel: function (col = 0, x = 0, y = 0) {
+		putPixel: function ([r,g,b], x = 0, y = 0) {
 			let idx = (y * this.w + x) * 4,
 				img = this.imgData.data;
 
-			img[idx] = col * 0.5;
-			img[idx + 1] = col;
-			img[idx + 2] = col * 0.5;
+			img[idx] = r;
+			img[idx + 1] = g;
+			img[idx + 2] = b;
 			img[idx + 3] = 255;
 		},
 
 		update: function () {
 			let {imgData, env, w, h} = this,
 				img = imgData.data,
-				clamp = (v) => Math.min(1, Math.max(0, v));
+				clamp = (v) => Math.min(1, Math.max(0, v)),
+				scale = (v) => v * 255;
 
 			for (let {x, y} of matrix(w, h)) {
 
 				let idx = (x + (y * w)) * 4,
 					pos = { idx: idx, x: x, y: y, xr: x / w, yr: y / h };
 
-				let mixed = this.funcs.slice(0, 2).reduce((ac, f) => f(
-					clamp(ac),
+				let mixed = this.funcs.reduce((ac, f) => f(
+					ac.map(clamp),
 					pos,
 					env
-				), img[idx]);
+				), [img[idx], img[idx + 1], img[idx + 2]]);
 
-				this.putPixel(clamp(mixed) * 255, x, y);
+				this.putPixel(mixed.map(clamp).map(scale), x, y);
 
 			}
 
